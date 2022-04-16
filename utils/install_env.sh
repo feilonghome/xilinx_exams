cdbeeb96eb53979d0c5407499b64d8cb5427deff
yaoyulong <feilongphone@gmail.com>
2022-04-16 17:41:53 +0800

    util: add cmake to install script

--- utils/install_env.sh
+++ utils/install_env.sh
@@ -2,7 +2,7 @@
 
 apt update
 apt upgrade -y
-apt install -y vim tmux fish clang clang-format golang build-essential
+apt install -y vim tmux fish clang clang-format golang build-essential cmake
 
 # install nodejs 16 by nvm
 
